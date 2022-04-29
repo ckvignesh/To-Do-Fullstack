e9ddd99bc53abc848c6644990b013a889acdc7ef
ckvignesh <vigneshckprakash@gmail.com>
2022-04-29 12:42:24 +0530

    basic server created

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("server running on local host 5000");
+});
