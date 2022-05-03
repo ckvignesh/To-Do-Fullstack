d99b06e3f6114fdc8d5bc9b2153978a475f69faf
ckvignesh <vigneshckprakash@gmail.com>
2022-05-03 23:48:44 +0530

    routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
 app.listen(5000, () => {
   console.log("server running on local host 5000");
@@ -14,7 +14,13 @@ app.listen(5000, () => {
 // routes
 
 // create todo
-
+app.post("/todos", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.log(err.messsage);
+  }
+});
 // get a todo
 
 // get all todo
